9d4643494e806acea7031fe47d7f9bf8b2bfd403
krutulkamil <krutulkamil@onet.pl>
2023-01-26 15:52:08 +0100

    feat: add infinite scroll with useInfiniteQuery

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,9 +1,10 @@
-import type { FunctionComponent } from "react";
+import { useEffect } from "react";
 import CreateTweet from "./CreateTweet";
 import Tweet from "./Tweet";
 import { api } from "../utils/api";
 import { useScrollPosition } from "../hooks/useScrollPosition";
 import { useDebounce } from "../hooks/useDebounce";
+import type { FunctionComponent } from "react";
 
 const LIMIT = 10;
 
@@ -17,6 +18,12 @@ const Timeline: FunctionComponent = (): JSX.Element => {
     getNextPageParam: (lastPage) => lastPage.nextCursor
   });
 
+  useEffect(() => {
+    if (debouncedScrollPosition > 90 && hasNextPage && !isFetching) {
+      fetchNextPage()
+    }
+  }, [fetchNextPage, hasNextPage, isFetching, debouncedScrollPosition]);
+
   const tweets = data?.pages.flatMap((page) => page.tweets) ?? [];
 
   return (
@@ -24,13 +31,7 @@ const Timeline: FunctionComponent = (): JSX.Element => {
       <CreateTweet />
       <div className="border-l-2 border-r-2 border-t-2 border-gray-500">
         {tweets.map((tweet) => <Tweet key={tweet.id} tweet={tweet} />)}
-
-        <button
-          onClick={() => fetchNextPage()}
-          disabled={!hasNextPage || isFetching}
-        >
-          load next
-        </button>
+        {!hasNextPage && <p>No more items to load</p>}
       </div>
     </div>
   );
